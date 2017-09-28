d7b1c11f73187d66fa80dd1d9d5d461ea19e5c41
victor141516 <yomellamovictor@gmail.com>
2017-09-28 20:06:49 +0200

    Publish on github

--- config.py
+++
@@ -1,6 +0,0 @@
-BOT_NAME = "g_imagebot"
-CSE_KEY = "AIzaSyAgaWiuUSMyx2rpXNCM7cjqj70g4uJpHPg"
-CSE_CX = "017839625266631737468:49zyrnlnquw"
-API_TOKEN = "433849602:AAHYvB5-EzsWp7-tiOD9yBRY6jffZxxoGIw"
-BATCH = 10
-WEBHOOK_URL = "https://google-image-search-telegram.herokuapp.com"
---
+++ config_example.py
@@ -0,0 +1,6 @@
+BOT_NAME = "g_imagebot"
+CSE_KEY = ""
+CSE_CX = ""
+API_TOKEN = ""
+BATCH = 10
+WEBHOOK_URL = ""
